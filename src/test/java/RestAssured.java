95c13ed6ecf756119d141a5d73ba6f580b5f1bcd
[200~ <snagy@noon.com>
2025-01-30 18:11:49 +0200

    first commit

---
+++ src/test/java/RestAssured.java
@@ -0,0 +1,73 @@
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+import static io.restassured.RestAssured.given;
+
+public class RestAssured {
+    AndroidDriver driver;
+    @BeforeTest
+    public void SetUp() throws MalformedURLException {
+        DesiredCapabilities caps=new DesiredCapabilities();
+        caps.setCapability("platformName","Android");
+        caps.setCapability("appium:platformVersion","10");
+        caps.setCapability("appium:deviceName","EDA51K");
+        caps.setCapability("appium:app","C:\\Users\\snagy\\Downloads\\HubApp.apk");
+        //  caps.setCapability("browserName","Chrome");
+        driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), caps);
+
+
+    }
+    @Test
+    public void CreateTripApi() {
+        String cookieName = "Cookie";
+        String cookieValue = "_ntrtnetid=nav1.public.eyJob3N0cyI6W10sInJvbGVzIjpbImxtcy1taWRkbGVtaWxlLXRyaXAtZWRpdG9yIiwibG1zLW1pZGRsZW1pbGUtYWRtaW4iLCJsbXMtbWlkZGxlbWlsZS1zdXBlcnZpc29yIiwiZGV2ZWxvcGVyIiwidGVjaC1lZ3lwdCIsImxtcy1hZG1pbiIsImxtcy1taWRkbGVtaWxlLXJvdXRlLWVkaXRvciIsInRlY2gtZWd5cHQtcWEiLCJsbXMtbGFzdG1pbGUtYWRtaW4iLCJmaW5hbmNlLXJlYWRvbmx5Il0sImVtYWlsIjoic25hZ3lAbm9vbi5jb20iLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUNnOG9jSzJtdUp3RWRpaEhfdHFELXFEMVQxb01Pckk4RE1Tc0JBY0lHc193blFxNmFwak5LOU09czk2LWMiLCJpYXQiOjE3MzgyMjY5NDEsInR5cGUiOiJkb21haW4iLCJ0aWQiOiI5M2U2NWRmNS0xZjQyLTQzNDgtYmVmMC1jMzY4MjA5YzU5ZDQiLCJzaWQiOiIwYThiNTVhNi04Y2Q4LTQxYzQtOTE1MS0zZGI3YTNiMmY1MjEifUIxbncvZVE4Y2Mvc1RadU83bnFuT1pwV21qU1dla2FEK1hFaGdVdG9HNVRLZFkzbHpGOVFrcmFpZ1ZvbzJSOUVVTjU1elNmcEIrK0dQZ2QzYy9lSGZVdDhMNFgrM1UxV2piSUpLb1lubTRUWlo1OFM0Ukc0OW01VVdRRitnMUttVHowRXA1Q2c0QWhBNG1OSHdNM1Fyc0pXcjZFd3p1UXBNTGVTSjUwNWd1VFFGMWdWSkM3eW92TTZjekZEKzE0dFYwWCtxMC9yTHQrQVhxWC9JR1RkcHVxazUzcmcza1B1cWlxU2ZYeUF2MnBJVG5jM1gxWW5YR2krR1g0RW1lb3F0Q2pRdXUvckhBOExuMnp1SGcvV1k1WXYzemdHNTBzT29PWmcrK01abTkxQVk5U1laMmYvb0pybXp6cHVKR3dQckJ5SUU3ZFAwYkNjRVlIdy9sUnJiNmFqbnpUZjRxSkdqalZyenNsa1dBTXZNTkhodWdQQkk1bjNyWWFkaEwwNXFDNUVyODNITy9ScWlqTjN3NUp3MnpaQ2NMcWhJcWthYjlmQnluS25QNWN1L1AwQlVJWjVucXRnOGxWSUFVTHFRWWtKRDJiQjVqVGZlN3doM2h4U25CYTR3TFh0Slk4K1VLT1A4WCt4S3gwcGo3Q2hNNENRNXI3U0haOFYwNzk2.MQ==; _ntsid=0a8b55a6-8cd8-41c4-9151-3db7a3b2f521; _dd_s=logs=1&id=6c4c3401-12a9-436f-90d8-070cc0854a5c&created=1738241178468&expire=1738244255956";
+        String CreateTripPayload = "{\n" +
+                "  \"stage\": \"trip-created\",\n" +
+                "  \"stops\": [\"TEST-A1\", \"TEST-A2\", \"TEST-A3\"],\n" +
+                "  \"vendor\": \"noon\",\n" +
+                "  \"vehicle_type\": \"canter_4tn\"\n" +
+                "}";
+        Response response = given()
+                .header("Accept", "application/json")
+                .header("Content-Type", "application/json")
+                .header(cookieName, cookieValue)
+                .body(CreateTripPayload)
+                .when()
+                .post("https://sc-express-api-middlemile.noonstg.team/staging/test-data/trip");
+
+        System.out.println("Raw Response Body: " + response.getBody().asString());
+        System.out.println("Response Status Code: " + response.getStatusCode());
+        System.out.println("Response Content-Type: " + response.getContentType());
+
+        JsonPath jsonPath = response.jsonPath();
+        String tripCode = jsonPath.getString("code");
+        System.out.println("Extracted Trip Code: " + tripCode);
+
+
+    }
+    @AfterTest
+    public void tearDown(){
+        if (driver != null) {
+            driver.quit();
+            System.out.println("Driver quit in hooks: " + driver);
+            driver = null; // Reset driver to null after quitting
+        }
+    }
+}
